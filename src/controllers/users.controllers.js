910fc72843d268a08744259957f77743b14fc4b0
Lúcio Bruno <luciobrunop20@hotmail.com>
2023-04-27 17:51:39 -0400

    feat:login

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,5 +1,6 @@
 import { db } from "../database/database.connection.js";
 import bcrypt from "bcrypt"
+import { v4 as uuid } from "uuid"
 
 export async function signup(req, res) {
     const { name, email, password } = req.body
@@ -15,6 +16,28 @@ export async function signup(req, res) {
 
         await db.collection("users").insertOne({ name, email, password: hash })
         res.sendStatus(201)
+
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+}
+
+export async function signin(req, res) {
+    const { email, password } = req.body
+
+    try {
+        const user = await db.collection("users").findOne({ email })
+        if (!user) {
+            return res.status(404).send("E-mail não cadastrado")
+        }
+        const correctPassword = bcrypt.compareSync(password, user.password)
+        if (!correctPassword) {
+            return res.status(401).send("Senha incorreta")
+        }
+
+        const token = uuid()
+        await db.collection("sessions").insertOne({ token, idUser: user._id })
+        res.status(200).send({ name: user.name, token })
 
     } catch (err) {
         res.status(500).send(err.message)
